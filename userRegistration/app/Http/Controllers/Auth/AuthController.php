39f29f1ee42422b453aeca2ddc667ffed2095e86
Romil Shah <romils1994@gmail.com>
2021-06-01 22:42:05 -0400

    File Upload functionality Added

--- userRegistration/app/Http/Controllers/Auth/AuthController.php
+++ userRegistration/app/Http/Controllers/Auth/AuthController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Session;
 use App\Models\User;
 use Hash;
+
 
 class AuthController extends Controller
 {
@@ -31,12 +33,18 @@ class AuthController extends Controller
         if(Auth::check()){
             $userDetails = Auth::user();
             $data = [
+                'userid' => $userDetails->id,
                 'username' => $userDetails->username,
                 'firstname' => $userDetails->firstname,
                 'lastname' => $userDetails->lastname,
                 'email' => $userDetails->email
             ];
 
+            $filename = 'word-counter-files/' . $userDetails->id . '.txt';
+            $s3Storage = Storage::disk('s3');
+            if ($s3Storage->has($filename)) {
+                $data['wordCount'] = str_word_count($s3Storage->get($filename));
+            }
             return view('detail',$data);
         }
 
@@ -72,7 +80,7 @@ class AuthController extends Controller
             return redirect()->intended($redirect);
         }
 
-        return redirect("login")->withSuccess('Oops! You have entered invalid credentials');
+        return redirect('login')->withSuccess('Oops! You have entered invalid credentials');
     }
 
     /**
@@ -83,14 +91,31 @@ class AuthController extends Controller
     public function postRegistration(Request $request)
     {
         $request->validate([
-            'username' => 'required|max:255',
+            'username' => 'required|max:255|unique:users',
             'password' => 'required|min:6'
         ]);
 
         $data = $request->all();
         $check = $this->create($data);
 
-        return redirect('details');
+        $credentials = $request->only('username', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $userDetArr = Auth::user();
+            $data = [
+                'username' => $userDetArr->username,
+                'firstname' => $userDetArr->firstname,
+                'lastname' => $userDetArr->lastname,
+                'email' => $userDetArr->email
+            ];
+            $redirect = 'dashboard';
+            if (!$data['firstname'] || !$data['lastname'] || !$data['email']) {
+                $redirect = 'details';
+            }
+            return redirect()->intended($redirect);
+        }
+
+        return redirect('login')->withSuccess('Oops! You have entered invalid credentials');
     }
 
     /**
@@ -103,8 +128,16 @@ class AuthController extends Controller
         $request->validate([
             'firstname' => 'required|max:255',
             'lastname' => 'required|max:255',
-            'email' => 'required|email|max:255'
+            'email' => 'required|email|max:255',
+            'textfile' => 'mimetypes:text/plain|mimes:txt'
         ]);
+
+        if ($request->hasFile('textfile')) {
+            $file = $request->file('textfile');
+            $name = Auth::user()->id . '.txt';
+            $filePath = 'word-counter-files/' . $name;
+            Storage::disk('s3')->put($filePath, file_get_contents($file));
+        }
 
         $data = $request->all();
         $check = $this->update($data);
@@ -125,8 +158,13 @@ class AuthController extends Controller
                 'username' => $userDetails->username,
                 'firstname' => $userDetails->firstname,
                 'lastname' => $userDetails->lastname,
-                'email' => $userDetails->email
+                'email' => $userDetails->email,
             ];
+            $filename = 'word-counter-files/' . $userDetails->id . '.txt';
+            $s3Storage = Storage::disk('s3');
+            if ($s3Storage->has($filename)) {
+                $data['wordCount'] = str_word_count($s3Storage->get($filename));
+            }
 
             return view('dashboard',$data);
         }
